76ddbfe1412d7ddab93873eac51cf1b9776b4dcf
Tobias Gehring <mail@tobiasgehring.de>
2025-02-07 14:57:05 +0100

    withot uart code files

--- ctrlVOA.py
+++ ctrlVOA.py
@@ -3,31 +3,32 @@ import RPi.GPIO as GPIO
 import time
 import configparser
 
+
 # required to do in the beginning to ensure the motors are not drawing too much current
 def assert_ENpins_low(pin_en_list):
     GPIO.setmode(GPIO.BCM)
     for pin in pin_en_list:
+        try:
             GPIO.setup(pin, int(GpioMode.OUT), initial=int(Gpio.LOW))
+        except RuntimeWarning:
+            print(f'RuntimeWarning came from pin {pin}')
 
-def set_motor(tmc, res_mstep):
-    tmc.set_direction_reg(False)
-    tmc.set_current(300)
-    tmc.set_interpolation(True)
-    tmc.set_spreadcycle(False)
-    tmc.set_microstepping_resolution(res_mstep)
-    tmc.set_internal_rsense(False)
-    tmc.set_acceleration_fullstep(1000)
-    tmc.set_max_speed_fullstep(250)
+
+def make_INDpins_inp(pin_ind_list):
+    GPIO.setmode(GPIO.BCM)
+    for pin in pin_ind_list:
+        GPIO.setup(pin, int(GpioMode.IN))
+
 
 def run_motor(tmc, lin_range=0):
     tmc.set_motor_enabled(True)
     if lin_range == 0:
         ni = 3
         for i in range(ni):
-            print(f'Iteration {i+1} of {ni} starting...')
+            print(f'Iteration {i + 1} of {ni} starting...')
             tmc.run_to_position_steps(9000, MovementAbsRel.RELATIVE)
             tmc.run_to_position_steps(-9000, MovementAbsRel.RELATIVE)
-            print(f'Iteration {i+1} of {ni} finished, waiting for 1 sec')
+            print(f'Iteration {i + 1} of {ni} finished, waiting for 1 sec')
             time.sleep(1)
     else:
         print(f'Moving by {lin_range} steps')
@@ -42,18 +43,20 @@ if __name__ == "__main__":
     cfg = configparser.ConfigParser(comment_prefixes='#', inline_comment_prefixes='#')
     cfg.read('ctrlVOA.conf')
     list_enable_pins = cfg['voa']['enables'].split(',')
-    list_uart_addrs = cfg['voa']['addresses'].split(',')
+    list_index_pins = cfg['voa']['indexes'].split(',')
     chosen_voa_ix = int(cfg['voa']['id']) - 1  # -1 so that index starts from 0
     en_pins_all = [int(pin) for pin in list_enable_pins]
+    ind_pins_all = [int(pin) for pin in list_index_pins]
     print(f'Setting all EN pins to low (to prevent motors from drawing too much current)')
     assert_ENpins_low(en_pins_all)
     time.sleep(1)
+    print(f'Setting all IND pins to be input')
+    make_INDpins_inp(ind_pins_all)
+    time.sleep(1)
 
     pin_en = int(list_enable_pins[chosen_voa_ix])  # GPIO pin for the chosen one!
-    drvr_addr = int(list_uart_addrs[chosen_voa_ix])
     pin_step = int(cfg['common']['step'])
     pin_dir = int(cfg['common']['direction'])
-    res_microstep = int(cfg['movement']['micro_res'])
     try:
         steps_to_move = int(cfg['movement']['num_steps'])
     except ValueError:
@@ -68,7 +71,7 @@ if __name__ == "__main__":
     # initiate the TMC_2209 class
     # use your pins for pin_en, pin_step, pin_dir here
     # -----------------------------------------------------------------------
-    tmc = TMC_2209(pin_en, pin_step, pin_dir, driver_address=drvr_addr, loglevel=Loglevel.DEBUG)
+    tmc = TMC_2209(pin_en, pin_step, pin_dir, loglevel=Loglevel.DEBUG, skip_uart_init=True)
 
     # -----------------------------------------------------------------------
     # set the loglevel of the libary (currently only printed)
@@ -79,55 +82,10 @@ if __name__ == "__main__":
     tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
 
     # -----------------------------------------------------------------------
-    # these functions change settings in the TMC register
-    # -----------------------------------------------------------------------
-    tmc.set_direction_reg(False)
-    tmc.set_current(300)
-    tmc.set_interpolation(True)
-    tmc.set_spreadcycle(False)  # False will activate Stealthchop
-    tmc.set_microstepping_resolution(res_microstep)
-    tmc.set_internal_rsense(False)
-
-    print("---\n---")
-
-    # -----------------------------------------------------------------------
-    # these functions read and print the current settings in the TMC register
-    # -----------------------------------------------------------------------
-    tmc.read_ioin()
-    tmc.read_chopconf()
-    tmc.read_drv_status()
-    tmc.read_gconf()
-
-    print("---\n---")
-
-    # -----------------------------------------------------------------------
-    # set the Acceleration and maximal Speed
-    # -----------------------------------------------------------------------
-    # tmc.set_acceleration(2000)
-    # tmc.set_max_speed(500)
-
-    # -----------------------------------------------------------------------
     # set the Acceleration and maximal Speed in fullsteps
     # -----------------------------------------------------------------------
-    tmc.set_acceleration_fullstep(1000)
-    tmc.set_max_speed_fullstep(250)
-
-    # -----------------------------------------------------------------------
-    # Get various parameters before enabling the motor
-    # -----------------------------------------------------------------------
-    time.sleep(0.2)
-    dir_reg = tmc.get_direction_reg()
-    interp = tmc.get_interpolation()
-    resol_microstep_drv_reg = tmc.read_microstepping_resolution()
-    resol_microstep_cached = tmc.get_microstepping_resolution()
-    accel = tmc.get_acceleration()
-    max_speed = tmc.get_max_speed()
-    print(f'get_direction_reg() = {dir_reg}')
-    print(f'get_interpolation() = {interp}')
-    print(f'read_microstepping_resolution() = {resol_microstep_drv_reg}')
-    print(f'get_microstepping_resolution() = {resol_microstep_cached}')
-    print(f'get_acceleration() = {accel}')
-    print(f'get_max_speed() = {max_speed}')
+    tmc.set_acceleration(1000)
+    tmc.set_max_speed(250)
 
     # -----------------------------------------------------------------------
     # activate the motor current output
@@ -141,10 +99,10 @@ if __name__ == "__main__":
         tmc.run_to_position_steps(steps_to_move, MovementAbsRel.RELATIVE)
     else:
         for i in range(4):
-            print(f'Iteration {i} starting...')
-            tmc.run_to_position_steps(12000, MovementAbsRel.RELATIVE)  # move 2400 steps forward
-            tmc.run_to_position_steps(-12000, MovementAbsRel.RELATIVE)  # move 2400 steps backward
-            print(f'Iteration {i} finished, waiting for 2 sec')
+            print(f'Iteration {i+1} starting...')
+            tmc.run_to_position_steps(2000, MovementAbsRel.RELATIVE)  # move 2400 steps forward
+            tmc.run_to_position_steps(-2000, MovementAbsRel.RELATIVE)  # move 2400 steps backward
+            print(f'Iteration {i+1} finished, waiting for 2 sec')
             time.sleep(2)
 
     # -----------------------------------------------------------------------
--- demo/demo_script_09_uartless.py
+++ demo/demo_script_09_uartless.py
@@ -28,14 +28,14 @@ print("---")
 # use your pins for pin_en, pin_step, pin_dir here
 #-----------------------------------------------------------------------
 if BOARD == Board.RASPBERRY_PI:
-    tmc = TMC_2209(21, 16, 20, loglevel=Loglevel.DEBUG, skip_uart_init=True)
+    tmc = TMC_2209(22, 5, 6, loglevel=Loglevel.DEBUG, skip_uart_init=True)
 elif BOARD == Board.RASPBERRY_PI5:
-    tmc = TMC_2209(21, 16, 20, loglevel=Loglevel.DEBUG, skip_uart_init=True)
+    tmc = TMC_2209(22, 5, 6, loglevel=Loglevel.DEBUG, skip_uart_init=True)
 elif BOARD == Board.NVIDIA_JETSON:
-    tmc = TMC_2209(13, 6, 5, loglevel=Loglevel.DEBUG, skip_uart_init=True)
+    tmc = TMC_2209(22, 5, 6, loglevel=Loglevel.DEBUG, skip_uart_init=True)
 else:
     # just in case
-    tmc = TMC_2209(21, 16, 20, loglevel=Loglevel.DEBUG, skip_uart_init=True)
+    tmc = TMC_2209(22, 5, 6, loglevel=Loglevel.DEBUG, skip_uart_init=True)
 
 
 
@@ -82,23 +82,27 @@ tmc.set_motor_enabled(True)
 
 
 
-#-----------------------------------------------------------------------
-# move the motor 1 revolution
-#-----------------------------------------------------------------------
-tmc.run_to_position_steps(400)                             #move to position 400
-tmc.run_to_position_steps(0)                               #move to position 0
+# #-----------------------------------------------------------------------
+# # move the motor 1 revolution
+# #-----------------------------------------------------------------------
+# tmc.run_to_position_steps(400)                             #move to position 400
+# tmc.run_to_position_steps(0)                               #move to position 0
+#
+#
+# tmc.run_to_position_steps(400, MovementAbsRel.RELATIVE)    #move 400 steps forward
+# tmc.run_to_position_steps(-400, MovementAbsRel.RELATIVE)   #move 400 steps backward
+#
+#
+# tmc.run_to_position_steps(400)                             #move to position 400
+# tmc.run_to_position_steps(0)                               #move to position 0
 
-
-tmc.run_to_position_steps(400, MovementAbsRel.RELATIVE)    #move 400 steps forward
-tmc.run_to_position_steps(-400, MovementAbsRel.RELATIVE)   #move 400 steps backward
-
-
-tmc.run_to_position_steps(400)                             #move to position 400
-tmc.run_to_position_steps(0)                               #move to position 0
-
-
-
-
+ni = 3
+for i in range(ni):
+    print(f'Iteration {i + 1} of {ni} starting...')
+    tmc.run_to_position_steps(9000, MovementAbsRel.RELATIVE)
+    tmc.run_to_position_steps(-9000, MovementAbsRel.RELATIVE)
+    print(f'Iteration {i + 1} of {ni} finished, waiting for 1 sec')
+    time.sleep(1)
 
 #-----------------------------------------------------------------------
 # deactivate the motor current output
